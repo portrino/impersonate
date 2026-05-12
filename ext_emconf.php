772a426ffd4389d4c018aa417a7b06acfbe953b8
Axel Böswetter <boeswetter@portrino.de>
2026-05-13 00:29:30 +0200

    [TASK] fix version for TER upload

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,10 +16,10 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '3.0.0',
+    'version' => '3.1.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.0.0-12.99.99',
+            'typo3' => '12.4.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
